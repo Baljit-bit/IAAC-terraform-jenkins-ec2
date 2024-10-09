f0ca98425f24ef1c4e3c2fa1a680bdb16da60d67
Baljit-bit <baljitk024@gmail.com>
2024-10-09 16:43:16 -0400

    Delete terraform-master-slave-ec2-jenkins directory

--- terraform-master-slave-ec2-jenkins/install_jenkinslave.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo amazon-linux-extras install epel -y
-sudo yum install -y java-1.8.0-openjdk
-sudo yum install httpd -y
--- terraform-master-slave-ec2-jenkins/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "jenkins_master_ip" {
-  value = aws_instance.master_app_server.public_ip
-}
-
-output "jenkins_slave_ip" {
-  value = aws_instance.slave_app_server.public_ip
-}
